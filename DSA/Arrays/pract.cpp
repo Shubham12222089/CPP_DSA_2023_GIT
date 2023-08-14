f27bed9817ca325e81f4bdab6ec21ece75d887d6
Shubham <shubhamjha3039@gmail.com>
2023-08-14 21:01:30 +0530

    friend function

--- DSA/Arrays/pract.cpp
+++ DSA/Arrays/pract.cpp
@@ -21,28 +21,62 @@ using namespace std;
 //reverse of an array:
 
 
-void reverse(int arr[], int n){
-    int start = 0;
-    int end = n-1;
-    while(start<=end){
-        swap(arr[start],arr[end]);
-        start++;
-        end--;
-    }
-    cout<<endl;
-}
+// void reverse(int arr[], int n){
+//     int start = 0;
+//     int end = n-1;
+//     while(start<=end){
+//         swap(arr[start],arr[end]);
+//         start++;
+//         end--;
+//     }
+//     cout<<endl;
+// }
 
-void printArray(int arr[], int n){
+// void printArray(int arr[], int n){
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout<< arr[i] <<" ";
+//     }
+    
+// }
+
+// int main(){
+//     int arr[6] = {1,2,3,4,5,6};
+//     reverse(arr, 6);
+
+//     printArray(arr, 6);
+// }
+
+void even(int arr[], int n){
+    bool temp = false;
     for (int i = 0; i < n; i++)
     {
-        cout<< arr[i] <<" ";
+        if (arr[i]%2==0)
+        {
+            temp = true;
+            break;
+        }
+        
+    }
+    if(temp){
+        cout<<"Even number present in the array.";
+    }
+    else{
+        cout<<"Even number is not present in the array.";
     }
     
+    
 }
-
 int main(){
-    int arr[6] = {1,2,3,4,5,6};
-    reverse(arr, 6);
+    int n,temp=0;
+    cout<<"Enter the size: ";
+    cin>>n;
+    int arr[100];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    even(arr, n);
 
-    printArray(arr, 6);
+    
 }
---
+++ c++/05_Function/05_friend_function.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+using namespace std;
+
+// class complex{
+//     int a, b;
+
+//     public:
+//     void setNumber(int num1, int num2){
+//         a = num1;
+//         b = num2;
+//     }
+
+//     void print(){
+//         cout << "Your number is: " << a << " + " << b << "i" <<endl;
+//     }
+//     friend complex sumComplex(complex , complex);
+// };
+
+// complex sumComplex(complex obj1, complex obj2){
+//     complex result;
+//     result.setNumber((obj1.a + obj2.a), (obj1.b+obj2.b));
+//     return result;
+// } 
+
+// int main(){
+//     complex c1, c2, sum;
+//     c1.setNumber(1,2);
+//     c2.setNumber(2,3);
+//     c1.print();
+//     c2.print();
+
+//     sum = sumComplex(c1,c2);
+//     sum.print();
+// }
+
+//friend function property:
+//has access to private and protected members
+//friend function cannot be called by objects of complex class
+//cannot access 
+
+
+// Define a class Rectangle with private attributes length 
+// and width. Create a friend function named calculateArea 
+// that calculates the area of the rectangle.
+
+// class Rectangle{
+//     int length;
+//     int width;
+
+//     public:
+//     void area(int num1, int num2){
+//         length = num1;
+//         width = num2;
+//     }
+
+//     int calculateArea(){
+//         return length * width;
+//     }
+//     friend Rectangle Areavalue(Rectangle, Rectangle);
+// };
+
+// Rectangle Areavalue(Rectangle obj1, Rectangle obj2){
+//     Rectangle result;
+//     result.area(obj1.length,obj2.width);
+//     return result;
+    
+// }
+// int main(){
+//     Rectangle c1, alg;
+//     c1.area(4,8);
+//     c2.area(5,6);
+//     c1.calculateArea();
+//     c2.calculateArea();
+
+//     alg = Areavalue(c1,c2);
+//     int combined = alg.calculateArea();
+//     cout<<combined;
+
+// }
+
+
+//-----------------------
+
+class complex{
+    int a,b;
+
+    public:
+    void setnumber(int num1, int num2){
+        a = num1;
+        b = num2;
+    }
+    void print(){
+        cout << "Result is : " << a <<" + "<<b<<"i"<<endl;
+    }
+
+    friend class Calculator;
+};
+
+class Calculator{
+    public:
+    int sumreal(complex obj1, complex obj2){
+        return obj1.a + obj2.a;
+    }
+};
+
+int main(){
+    complex o1,o2;
+    o1.setnumber(1,2);
+    o2.setnumber(2,3);
+
+    Calculator calc;
+    int alg = calc.sumreal(o1,o2);
+    cout << "real sum is: "<<alg;
+
+}
