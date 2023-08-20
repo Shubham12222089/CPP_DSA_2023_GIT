d653478795f38ece209990cd114ed125740a2d5e
Shubham <shubhamjha3039@gmail.com>
2023-08-20 23:18:21 +0530

    sorting

--- c++/07_Classes/02_example.cpp
+++ c++/07_Classes/02_example.cpp
@@ -1,30 +1,40 @@
 #include<iostream>
 using namespace std;
 
-class room{
+// class room{
+//     public:
+
+//     int length;
+//     int breadth;
+//     int height;
+
+//     int calculateArea(){
+//         return length * breadth;
+//     }
+
+//     int calculateVolume(){
+//         return length * breadth * height;
+//     }
+
+// };
+
+// int main(){
+//     room room1;//object bnaya
+
+//     room1.length = 10;
+//     room1.breadth = 10;
+//     room1.height = 5;
+
+//     cout<< room1.calculateArea()<<endl;
+//     cout<< room1.calculateVolume();
+// }
+class Test{
     public:
-
-    int length;
-    int breadth;
-    int height;
-
-    int calculateArea(){
-        return length * breadth;
-    }
-
-    int calculateVolume(){
-        return length * breadth * height;
-    }
+    int x;
 
 };
-
 int main(){
-    room room1;//object bnaya
-
-    room1.length = 10;
-    room1.breadth = 10;
-    room1.height = 5;
-
-    cout<< room1.calculateArea()<<endl;
-    cout<< room1.calculateVolume();
+    Test t;
+    cout<<t.x;
+    return 0;
 }
