16dc3a6969a6ac488f5baa98f5f0cecc422c75e9
Shubham <shubhamjha3039@gmail.com>
2023-09-22 23:40:04 +0530

    s

--- DSA/01_Arrays/01_Arrays/03_binary_search.cpp
+++ DSA/01_Arrays/01_Arrays/03_binary_search.cpp
@@ -1,102 +1,102 @@
 //binary search is monotonic function.
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int binary_search(int n,int arr[],int key){
-    int start = 0;
-    int end = n-1;
-    int mid = (start+end)/2;
-    while(start<=end){
-        //mid
-        if(arr[mid]==key){
-        return mid;
-        }
-        else if(arr[mid]>key){
-            //right
-            start = mid+1;
-        }
-        else{
-            //left
-            end = mid - 1; 
-        }
-        mid = (start + end)/2; //updated mid.
-    }
-    return -1;
+// int binary_search(int n,int arr[],int key){
+//     int start = 0;
+//     int end = n-1;
+//     int mid = (start+end)/2;
+//     while(start<=end){
+//         //mid
+//         if(arr[mid]==key){
+//         return mid;
+//         }
+//         else if(arr[mid]>key){
+//             //right
+//             start = mid+1;
+//         }
+//         else{
+//             //left
+//             end = mid - 1; 
+//         }
+//         mid = (start + end)/2; //updated mid.
+//     }
+//     return -1;
 
-}
+// }
 
-int main(){
-    int key,n;
-    cout<<"Enter the size: ";
-    cin>>n;
-    int arr[100];
-    for(int i = 0;i<n;i++){
-        cin>>arr[i];
-    }
-    cout<<"Enter the key: ";
-    cin>>key;
-    int temp = binary_search(n,arr,key);
-    cout<<"index of "<<key<<" is: "<<temp<<endl;
+// int main(){
+//     int key,n;
+//     cout<<"Enter the size: ";
+//     cin>>n;
+//     int arr[100];
+//     for(int i = 0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     cout<<"Enter the key: ";
+//     cin>>key;
+//     int temp = binary_search(n,arr,key);
+//     cout<<"index of "<<key<<" is: "<<temp<<endl;
 
-}
+// }
 
 
 
 //
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int firstOccb(int arr[],int n,int key){
-//     int start = 0;
-//     int end = n-1;
-//     int mid = (start + end)/2;
-//     int ans = -1;
+int firstOccb(int arr[],int n,int key){
+    int start = 0;
+    int end = n-1;
+    int mid = (start + end)/2;
+    int ans = -1;
 
-//     while(start<=end){
-//         if(arr[mid] == key){
-//             ans = mid;
-//             end = mid-1;
-//         }
-//         else if(key>arr[mid]){
-//             start = mid+1;
-//         }
-//         else{
-//             end = mid-1;
-//         }
-//         mid = (start+end)/2;
-//     }
-//     return ans;
-// }
+    while(start<=end){
+        if(arr[mid] == key){
+            ans = mid;
+            end = mid-1;
+        }
+        else if(key>arr[mid]){
+            start = mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+        mid = (start+end)/2;
+    }
+    return ans;
+}
 
-// int lastOccr(int arr[],int n,int key){
-//     int start = 0;
-//     int end = n-1;
-//     int mid = (start + end)/2;
-//     int ans = -1;
-//     while(start<=end){
-//         if(arr[mid] == key){
-//             ans = mid;
-//             start = mid+1;
-//         }
-//         else if(key>arr[mid]){
-//             start = mid+1;
-//         }
-//         else{
-//             end = mid-1;
-//         }
-//         mid = (start+end)/2;
-//     }
-//     return ans;
-// }
+int lastOccr(int arr[],int n,int key){
+    int start = 0;
+    int end = n-1;
+    int mid = (start + end)/2;
+    int ans = -1;
+    while(start<=end){
+        if(arr[mid] == key){
+            ans = mid;
+            start = mid+1;
+        }
+        else if(key>arr[mid]){
+            start = mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+        mid = (start+end)/2;
+    }
+    return ans;
+}
 
-// int main(){
-//     int arr[5] = {1,2,3,3,5};
+int main(){
+    int arr[5] = {1,2,3,3,5};
 
-//     cout<<"First Occurance of 3 is: "<< firstOccb(arr,5,3)<<endl;
-//     cout<<"Last Occurance of 3 is: "<<lastOccr(arr,5,3);
+    cout<<"First Occurance of 3 is: "<< firstOccb(arr,5,3)<<endl;
+    cout<<"Last Occurance of 3 is: "<<lastOccr(arr,5,3);
 
 
-// }
+}
 
 // #include<iostream>
 // using namespace std;
--- DSA/03_Stack/05_problem_4.cpp
+++ DSA/03_Stack/05_problem_4.cpp
@@ -1,83 +1,127 @@
-#include<iostream>
-#include<stack>
-#include<cmath>
+// #include<iostream>
+// #include<stack>
+// #include<cmath>
+// using namespace std;
+// //we are iterating loop in reverse order:
+// int prefix(string s){
+
+//     stack<int> st;
+
+//     for (int i = s.length()-1; i >= 0; i--)
+//     {
+//         if(s[i]>='0' && s[i] <= '9'){//s is string so we have to do this.
+//             st.push(s[i] - '0');
+//         }
+//         else{//if i have an operator
+//             int operand1 = st.top();
+//             st.pop();
+//             int operand2 = st.top();
+//             st.pop();
+//             switch (s[i]){
+//                 case '+':
+//                     st.push(operand1 + operand2);
+//                     break;
+//                 case '-':
+//                     st.push(operand1-operand2);
+//                     break;
+//                 case '*':
+//                     st.push(operand1 * operand2);
+//                     break;
+//                 case '/':
+//                     st.push(operand1/operand2);
+//                     break;
+//                 case '^':
+//                     st.push(pow(operand1, operand2));
+//                     break;
+//             }
+
+//         }
+//     }
+//     return st.top();
+// }
+// int postfix(string s){
+//     stack<int> st;
+
+//     for (int i = 0; i < s.length(); i++)
+//     {
+//         if(s[i]>='0' && s[i] <= '9'){
+//             st.push(s[i] - '0');
+//         }
+//         else{//if i have an operator
+//             int operand1 = st.top();
+//             st.pop();
+//             int operand2 = st.top();
+//             st.pop();
+//             switch (s[i]){
+//                 case '+':
+//                     st.push(operand1 + operand2);
+//                     break;
+//                 case '-':
+//                     st.push(operand1-operand2);
+//                     break;
+//                 case '*':
+//                     st.push(operand1 * operand2);
+//                     break;
+//                 case '/':
+//                     st.push(operand1/operand2);
+//                     break;
+//                 case '^':
+//                     st.push(pow(operand1, operand2));
+//                     break;
+//             }
+
+//         }
+//     }
+//     return st.top();
+// }
+// int main(){
+//     string s="-+7*45+20";
+//     string p="42*3+";
+//     cout<<prefix(s)<<endl;
+//     cout<<postfix(p)<<endl;
+
+// }
+
+
+//-------------------------------------------------------
+
+#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-//we are iterating loop in reverse order:
-int prefix(string s){
 
-    stack<int> st;
-
-    for (int i = s.length()-1; i >= 0; i--)
-    {
-        if(s[i]>='0' && s[i] <= '9'){//s is string so we have to do this.
-            st.push(s[i] - '0');
-        }
-        else{//if i have an operator
-            int operand1 = st.top();
-            st.pop();
-            int operand2 = st.top();
-            st.pop();
-            switch (s[i]){
-                case '+':
-                    st.push(operand1 + operand2);
-                    break;
-                case '-':
-                    st.push(operand1-operand2);
-                    break;
-                case '*':
-                    st.push(operand1 * operand2);
-                    break;
-                case '/':
-                    st.push(operand1/operand2);
-                    break;
-                case '^':
-                    st.push(pow(operand1, operand2));
-                    break;
-            }
-
-        }
-    }
-    return st.top();
-}
-int postfix(string s){
-    stack<int> st;
-
-    for (int i = 0; i < s.length(); i++)
-    {
-        if(s[i]>='0' && s[i] <= '9'){
-            st.push(s[i] - '0');
-        }
-        else{//if i have an operator
-            int operand1 = st.top();
-            st.pop();
-            int operand2 = st.top();
-            st.pop();
-            switch (s[i]){
-                case '+':
-                    st.push(operand1 + operand2);
-                    break;
-                case '-':
-                    st.push(operand1-operand2);
-                    break;
-                case '*':
-                    st.push(operand1 * operand2);
-                    break;
-                case '/':
-                    st.push(operand1/operand2);
-                    break;
-                case '^':
-                    st.push(pow(operand1, operand2));
-                    break;
-            }
-
-        }
-    }
-    return st.top();
-}
-int main(){
-    string s="-+7*45+20";
-    string p="42*3+";
-    cout<<prefix(s)<<endl;
-    cout<<postfix(p)<<endl;
-
+int main() {
+	stack<char> st;
+	st.push('1');
+	st.push('2');
+	st.push('3');
+	st.push('4');
+	st.push('5');
+	st.push('6');
+	st.push('7');
+	vector<char>v;
+	while(!st.empty()){
+	v.push_back(st.top());
+	st.pop();
+	}
+	int n=v.size();
+	if(n%2==0){
+	int target=(n/2);
+	for(int i=0;i<n;i++){
+		if(i==target)continue;
+		st.push(v[i]);
+	}
+	}else{
+	int target=ceil(n/2);
+	for(int i=0;i<n;i++){
+		if(i==target)continue;
+		st.push(v[i]);
+	}
+	}
+	cout<<"Printing stack after deletion of middle: ";
+	while (!st.empty()) {
+		char p = st.top();
+		st.pop();
+		cout << p << " ";
+	}
+	return 0;
 }
